218d55d71a322c33c941779418b93d96982298bf
Vinogradov Vadi <vinogradov.vadi@gmail.com>
2025-01-07 12:29:35 +0300

    Add omitempty for Custom struct: EMAIL and PHONE
    Added the possibility to create receipts by one of the EMAIL or PHONE parameters (currently both parameters are mandatory)

--- yookassa/common/customer.go
+++ yookassa/common/customer.go
@@ -1,6 +1,6 @@
 package yoocommon
 
 type Customer struct {
-	Phone string `json:"phone"`
-	Email string `json:"email"`
+	Phone string `json:"phone,omitempty"`
+	Email string `json:"email,omitempty"`
 }
